06f2f718b7ea8e4bb2908cd8f7677902293977d6
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-12-28 20:15:18 -0600

    initial products controller

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,9 @@
+export const createProduct = (req, res) => { }
+
+export const getProducts = (req, res) => { res.json('getProducts') }
+
+export const getProductById = (req, res) => { }
+
+export const updateProductById = (req, res) => { }
+
+export const deleteProductById = (req, res) => { }
