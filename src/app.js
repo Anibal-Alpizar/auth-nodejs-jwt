d9e5489dce7a2dc5571c03f9b69aab40d450adb8
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-12-28 19:53:57 -0600

    info from the package.json

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 import express from 'express'
+import morgan from 'morgan'
+import { readFileSync } from 'fs'
+
+const pkg = JSON.parse(readFileSync('package.json', 'utf8'))
 
 const app = express()
 
-app.listen(4000, () => {
-    console.log(`Server is running on port 4000`)
+app.set('pkg', pkg)
+
+
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.json({
+        author: app.get('pkg').author,
+        description: app.get('pkg').description,
+        version: app.get('pkg').version
+    })
 })
+export default app
+
