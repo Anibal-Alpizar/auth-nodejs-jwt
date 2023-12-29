3f7fdef233ccdedd898b06fcf0fd4d59a62c2e3b
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-12-28 22:07:33 -0600

    create and get all products

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,6 +1,22 @@
-export const createProduct = (req, res) => { }
+import Product from '../models/Product.js'
 
-export const getProducts = (req, res) => { res.json('getProducts') }
+export const createProduct = async (req, res) => {
+
+    const { name, category, price, imgURL } = req.body
+    const newProduct = new Product({
+        name: name,
+        category: category,
+        price: price,
+        imgURL: imgURL
+    })
+    const productSaved = await newProduct.save()
+    res.status(201).json(productSaved)
+}
+
+export const getProducts = async (req, res) => {
+    const products = await Product.find()
+    res.status(200).json(products)
+}
 
 export const getProductById = (req, res) => { }
 
