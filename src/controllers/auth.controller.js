5209bb7d4cd68bf0332e3226aafda85249d43f44
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-12-29 22:04:02 -0600

    roles added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/User.js'
 import jwt from 'jsonwebtoken'
 import config from '../config.js'
+import Role from '../models/Role.js'
 
 export const signUp = async (req, res) => {
 
@@ -11,13 +12,25 @@ export const signUp = async (req, res) => {
         email,
         password: await User.encryptPassword(password),
     })
+
+    if (roles) {
+        const foundRoles = await Role.find({ name: { $in: roles } }) // find roles in database
+        newUser.roles = foundRoles.map(role => role._id) // save roles ids in newUser
+
+    } else {
+        const role = await Role.findOne({ name: "user" }) // if roles are not specified, assign user role by default
+        newUser.roles = [role._id]
+    }
+
+
     const savedUser = await newUser.save()
+
+    console.log(savedUser)
 
     // {data saved, secret word, object config}
     const token = jwt.sign({ id: savedUser._id }, config.SECRET, { expiresIn: 86400 /* 24 hours */ })
     res.status(200).json({ token })
 
-    res.json('signUp')
 
 }
 export const signIn = async (req, res) => {
