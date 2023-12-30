e1bfea0b9a1d808cf160506914c276939069aa10
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-12-29 19:07:40 -0600

    remove deprecated mongo

--- src/database.js
+++ src/database.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose'
 
 
 mongoose.connect("mongodb://localhost/companydb", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    //useNewUrlParser: true, // <- deprecated
+    // useUnifiedTopology: true, // <- deprecated
 })
     .then(db => console.log('DB is connected'))
     .catch(err => console.log(err))
