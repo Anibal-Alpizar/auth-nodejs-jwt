be18d1aef582b4efbbba59ac59e00cc80b86d8e1
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-12-29 19:07:21 -0600

    save and asign a jwt token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,25 @@
 import User from '../models/User.js'
+import jwt from 'jsonwebtoken'
+import config from '../config.js'
 
-export const signUp = async (req, res) => {}
-export const signIn = async (req, res) => {}
+export const signUp = async (req, res) => {
+
+    const { userName, email, password, roles } = req.body
+
+    const newUser = new User({
+        userName,
+        email,
+        password: await User.encryptPassword(password),
+    })
+    const savedUser = await newUser.save()
+
+    // {data saved, secret word, object config}
+    const token = jwt.sign({ id: savedUser._id }, config.SECRET, { expiresIn: 86400 /* 24 hours */ })
+    res.status(200).json({ token })
+
+    res.json('signUp')
+
+}
+export const signIn = async (req, res) => {
+    res.json('signIn')
+}
