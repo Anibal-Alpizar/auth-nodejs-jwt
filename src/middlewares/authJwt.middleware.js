6ae6a2b2e53bf4a6966e3a00b9b7c53b094e3b5c
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-12-30 21:41:52 -0600

    isModerator middleware created

--- src/middlewares/authJwt.middleware.js
+++ src/middlewares/authJwt.middleware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import config from '../config.js'
 import User from '../models/User.js'
+import Role from '../models/Role.js'
 // have token? 
 // verify if the user is admin, moderator or user
 
@@ -15,6 +16,7 @@ export const verifyToken = async (req, res, next) => {
 
         // 3. But if token exists, decode it
         const decoded = jwt.verify(token, config.SECRET) // config.SECRET help us to decode the token
+        req.userId = decoded.id // global variable to use in other middlewares
 
         // 4. Check if user exists in database (with decoded id)
         const user = await User.findById(decoded.id)
@@ -28,4 +30,19 @@ export const verifyToken = async (req, res, next) => {
     catch (error) {
         res.status(401).json({ message: "Unauthorized" })
     }
+}
+
+export const isModerator = async (req, res, next) => {
+    const user = await User.findById(req.userId)
+    const roles = await Role.find({ _id: { $in: user.roles } }) // $in : find all roles that are in user.roles
+    for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === 'moderator') {
+            next()
+            return
+        }
+    }
+    return res.status(403).json({ message: "Require Moderator Role" })
+}
+
+export const isAdmin = async (req, res, next) => {
 }
