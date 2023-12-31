cc1742536c0c1c062dd78ed594d294da3c3adfed
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-12-30 21:58:12 -0600

    posting and verifying middlewares

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import productsRoutes from './routes/products.routes.js'
 import authRoutes from './routes/auth.routes.js'
+import usersRoutes from './routes/user.routes.js'
 import { readFileSync } from 'fs'
 import { createRoles } from './libs/initialSetup.js'
 
@@ -26,6 +27,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productsRoutes)
 app.use('/api/auth', authRoutes)
+app.use('/api/users', usersRoutes)
 
 export default app
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,1 +1,12 @@
-import {Router} from 'express'
+import { Router } from 'express'
+import * as userController from '../controllers/user.controller.js'
+import { authJwt } from '../middlewares/index.js'
+
+const router = Router()
+
+router.post('/', [
+    authJwt.verifyToken,
+    authJwt.isAdmin,
+], userController.createUser)
+
+export default router
